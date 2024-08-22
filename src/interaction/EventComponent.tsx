c96067ba2c31af338b1029a5143c53decc3bdfe6
imwkdrh <imwkdrh7@naver.com>
2024-08-22 10:09:15 +0900

    2024-08-22 10:09

---
+++ src/interaction/EventComponent.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+// 이벤트 처리:
+// - 웹페이지(컴포넌트)가 사용자와의 상호작용을 받아들여 그것에 대한 처리를 하는 것
+// - React에서는 addEventListener 함수를 사용하지 않고 on이벤트 속성을 사용
+// - 
+
+export default function EventComponent() {
+  return (
+    <div>EventComponent</div>
+  )
+}
