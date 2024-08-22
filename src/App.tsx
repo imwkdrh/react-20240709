87ecda7cebaa89d3e1c6d9d7b04581bbf4286d0d
imwkdrh <imwkdrh7@naver.com>
2024-08-22 12:13:50 +0900

    2024-08-22 12:13

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,8 @@ import Properties from './component_manage/Properties';
 import ConditionalRender from './component_manage/example/ConditionalRender';
 import Example2 from './component_manage/example/Example2';
 import ListRender from './component_manage/example/ListRender';
+import EventComponent from './interaction/EventComponent';
+import StateComponent from './interaction/StateComponent';
 
 function App() {
   return (
@@ -17,8 +19,10 @@ function App() {
       {/* <TodoList /> */}
       {/* <Properties/> */}
       {/* <ConditionalRender/> */}
-      <Example2 />
+      {/* <Example2 /> */}
       {/* <ListRender/> */}
+      {/* <EventComponent/> */}
+      <StateComponent />
     </>
   );
 }
